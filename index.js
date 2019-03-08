2b0b512f20eb45ee0230ba1de1cd6fab17479e2b
Jonathan Ortiz <tani.l.ortiz@gmail.com>
2019-03-08 15:38:31 -0800

    preload images and bug fixes

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/* PRELOAD IMAGES */
+var screenshot1 = preloadImage("marketing_assets/iPhoneX2@2x.png");
+var screenshot2 = preloadImage("marketing_assets/iPhoneX@2x.png");
+var element1 = document.getElementById('left-iphone');
+var element2 = document.getElementById('right-iphone');
+element1.style.display = 'block';
+element2.style.display = 'block';
+element1.classList.add('animated', 'fadeInDown');
+element2.classList.add('animated', 'fadeInDown');
+
+function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+    return img;
+}
+
+/* WINDOW SCROLL LOGIC */
 var runAnimationOnce = true;
 var runScreenAnimation = true;
 var runScreenAnimation2 = true;
@@ -5,7 +22,7 @@ var navBarTrigger = false;
 var initialRun = true;  // this keeps track of the first run of a render
 $(window).scroll(function() {
     // number counter animation
-    if (isScrolledIntoView(document.getElementById('counter'))) {
+    if (isScrolledIntoView(document.getElementById('count_trig'))) {
         if (runAnimationOnce) {
             triggerCount();
             runAnimationOnce = false;
@@ -43,7 +60,7 @@ $(window).scroll(function() {
         element.classList.remove('animated', 'fadeOut');
         element.classList.add('animated', 'fadeIn');
         navBarTrigger = false;
-    } else if (!isScrolledOutOfView(document.getElementById('nav-trig')) && !navBarTrigger){
+    } else if (!isScrolledOutOfView(document.getElementById('nav-trig')) && !navBarTrigger) {
         console.log('visible');
         var element = document.getElementById('nav-background');
         element.classList.remove('animated', 'fadeIn');
